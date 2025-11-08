7196f2c1053fa895493fcadfe57895bd29cfe3c8
Hugo Paipa <128321306+Icaro1ronaldo@users.noreply.github.com>
2025-11-08 00:44:11 +0000

    correction set up

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,15 @@
+# Set the workspace folder variable
+workspaceFolder="/workspaces/solar_forecasting"
+cd ${workspaceFolder}
+
+# Activate the appropriate submodule(s)
+git submodule init
+git submodule update
+echo "Submodules initialized and updated."
+
+# Create the .venv and run poetry
+python3 -m venv ${workspaceFolder}/.venv
+source ${workspaceFolder}/.venv/bin/activate
+pipx install poetry
+poetry install --no-root
+pre-commit install
